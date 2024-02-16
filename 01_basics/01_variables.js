1f962a8e0ab11395986c72d25ee91e6cdb7d0d7f
Shivam dave <103759719+Davshiv20@users.noreply.github.com>
2024-02-16 09:27:23 +0000

    let,var,const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const name="Shivam"
+let emailId="shivam69@gmail.com"
+let accPwd="12345"
+accCity="Ahmedabad"
+
+
+
+emailId="shivam1234@gmail.com"
+accPwd="54321"
+accCity="Vellore"
+console.table([emailId,accPwd,accCity])
+
